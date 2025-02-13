fab5f09e1a1ff787a77d31ac796ac733e2541568
Omelia Iliffe <omelia.iliffe@wetaworkshop.co.nz>
2025-02-13 13:18:16 +1300

    add device to rp2040 example

--- examples/rp2040/src/main.rs
+++ examples/rp2040/src/main.rs
@@ -7,31 +7,40 @@
 mod transport;
 
 use bsp::entry;
-use defmt::*;
+use core::convert::Infallible;
+use core::time::Duration;
+use defmt::{todo, unreachable, *};
 use defmt_rtt as _;
 use panic_probe as _;
 
+use crate::transport::DynamixelSerial;
 use adafruit_kb2040 as bsp;
 use adafruit_kb2040::hal;
 use adafruit_kb2040::hal::fugit::RateExtU32;
-use adafruit_kb2040::hal::uart::{DataBits, StopBits, UartConfig};
+use adafruit_kb2040::hal::uart::{DataBits, StopBits, UartConfig, UartDevice, ValidUartPinout};
 use bsp::hal::{
 	clocks::{init_clocks_and_plls, Clock},
 	pac,
 	sio::Sio,
 	watchdog::Watchdog,
 };
+use dynamixel2::{Bus, Device, Instructions, ReadError, SerialPort, TransferError};
 use embedded_hal::delay::DelayNs;
-use dynamixel2::Bus;
-use crate::transport::DynamixelSerial;
+use embedded_hal::digital::OutputPin;
 const ID: u8 = 1;
 const LED_ADDRESS: u16 = 65;
 const BAUD_RATE: u32 = 57600;
+
+#[cfg(all(feature = "client", feature = "device"))]
+compile_error!("cannot have both client and device features enabled");
+#[cfg(all(not(feature = "client"), not(feature = "device")))]
+compile_error!("must have either client and device feature enabled");
+
 #[entry]
 fn main() -> ! {
 	info!("Program start");
 	let mut pac = pac::Peripherals::take().unwrap();
-	let core = pac::CorePeripherals::take().unwrap();
+	let _core = pac::CorePeripherals::take().unwrap();
 	let mut watchdog = Watchdog::new(pac.WATCHDOG);
 	let sio = Sio::new(pac.SIO);
 
@@ -46,18 +55,12 @@ fn main() -> ! {
 		&mut pac.RESETS,
 		&mut watchdog,
 	)
-		.ok()
-		.unwrap();
+	.ok()
+	.unwrap();
 
+	let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
-	let mut timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
-
-	let pins = bsp::Pins::new(
-		pac.IO_BANK0,
-		pac.PADS_BANK0,
-		sio.gpio_bank0,
-		&mut pac.RESETS,
-	);
+	let pins = bsp::Pins::new(pac.IO_BANK0, pac.PADS_BANK0, sio.gpio_bank0, &mut pac.RESETS);
 	// SETUP THE UART PINS
 	let uart_pins = (
 		// UART TX
@@ -76,18 +79,101 @@ fn main() -> ! {
 	let dir_pin = pins.d6.into_function();
 	// CREATE THE TRANSPORT
 	let transport = DynamixelSerial::new(uart, BAUD_RATE, dir_pin, timer);
-	// CREATE THE BUS
-	let mut bus = Bus::with_buffers(transport, [0; 200], [0; 200]).unwrap();
+
+	if cfg!(all(feature = "client", not(feature = "device"))) {
+		client_loop(transport, timer)
+	} else if cfg!(all(feature = "device", not(feature = "client"))) {
+		device_loop(transport)
+	} else {
+		unreachable!("either client or device feature must be enabled")
+	}
+}
+
+fn client_loop<P, D, DIR>(transport: DynamixelSerial<P, D, DIR>, mut timer: hal::Timer) -> !
+where
+	P: ValidUartPinout<D>,
+	D: UartDevice,
+	DIR: OutputPin<Error = Infallible>,
+{
+	// CREATE THE CLIENT
+	let mut client = Bus::with_buffers(transport, [0; 200], [0; 200]).unwrap();
 	loop {
 		info!("led on");
-		if let Err(e) = bus.write_u8(ID, LED_ADDRESS, 1) {
+		if let Err(e) = client.write_u8(ID, LED_ADDRESS, 1) {
 			error!("Error: {:?}", defmt::Debug2Format(&e));
 		}
 		timer.delay_ms(500);
 		info!("led off");
-		if let Err(e) = bus.write_u8(ID, LED_ADDRESS, 0) {
-			error!("Error: {:?}", defmt::Debug2Format(&e));
+		if let Err(e) = client.write_u8(ID, LED_ADDRESS, 0) {
+			error!("Error: {:?}", Debug2Format(&e));
 		}
 		timer.delay_ms(500);
 	}
+}
+
+fn device_loop<P, D, DIR>(transport: DynamixelSerial<P, D, DIR>) -> !
+where
+	P: ValidUartPinout<D>,
+	D: UartDevice,
+	DIR: OutputPin<Error = Infallible>,
+{
+	let mut device = Device::with_buffers(transport, [0; 200], [0; 200]).unwrap();
+	loop {
+		if let Err(e) = process_packet(&mut device) {
+			error!("{:?}", Debug2Format(&e))
+		}
+	}
+}
+
+fn process_packet<ReadBuffer, WriteBuffer, P, D, DIR>(
+	device: &mut Device<ReadBuffer, WriteBuffer, DynamixelSerial<P, D, DIR>>,
+) -> Result<(), TransferError<transport::Error>>
+where
+	P: ValidUartPinout<D>,
+	D: UartDevice,
+	DIR: OutputPin<Error = Infallible>,
+	WriteBuffer: AsRef<[u8]> + AsMut<[u8]>,
+	ReadBuffer: AsRef<[u8]> + AsMut<[u8]>,
+{
+	let packet = device.read(Duration::from_millis(10));
+	if matches!(&packet,
+		Err(ReadError::Io(e)) if DynamixelSerial::<P, D, DIR>::is_timeout_error(&e))
+	{
+		return Ok(());
+	}
+	let packet = packet?;
+	let id = packet.id;
+	if id != ID && id != 254 {
+		return Ok(());
+	}
+	match packet.instruction {
+		Instructions::Ping => {
+			// todo: this should wait for based on id for some amount of time
+			device.write_status(ID, 0, 3, |buffer| {
+				buffer[..2].copy_from_slice(&1020_u16.to_le_bytes()); // u16 MODEL NUMBER
+				buffer[2] = 1; //u8 FIRMWARE VERSION
+			})?;
+		},
+		Instructions::Read { address, length } => {
+			if let Some(data) = todo!("get your data for reading") {
+				device.write_status(ID, 0, length as usize, |buffer| {
+					buffer.copy_from_slice(data);
+				})?;
+			} else {
+				device.write_status_error(ID, 0x07)?;
+			}
+		},
+		Instructions::Write { address, parameters } => {
+			if todo!("perform the write") {
+				device.write_status_ok(ID)?;
+			} else {
+				device.write_status_error(ID, 0x07)?;
+			}
+		},
+		Instructions::Unknown { instruction, .. } => error!("Unknown instruction {:?}", instruction),
+		i => {
+			warn!("unimplemented instruction: {}", Debug2Format(&i))
+		},
+	};
+	Ok(())
 }
--- examples/rp2040/src/transport.rs
+++ examples/rp2040/src/transport.rs
@@ -1,90 +1,93 @@
-use core::convert::Infallible;
-use core::time::Duration;
 use adafruit_kb2040::hal;
 use adafruit_kb2040::hal::fugit::ExtU64;
-use adafruit_kb2040::hal::Timer;
 use adafruit_kb2040::hal::timer::Instant;
 use adafruit_kb2040::hal::uart::{Enabled, UartDevice, UartPeripheral, ValidUartPinout};
+use adafruit_kb2040::hal::Timer;
+use core::convert::Infallible;
+use core::time::Duration;
 use cortex_m::prelude::_embedded_hal_serial_Write;
-use embedded_hal::digital::OutputPin;
 use dynamixel2::SerialPort;
+use embedded_hal::digital::OutputPin;
 use nb::block;
 
 use hal::uart::ReadErrorType;
 
 #[derive(Debug)]
 pub enum Error {
-    UartReadError(ReadErrorType),
-    Timeout,
+	UartReadError(ReadErrorType),
+	Timeout,
 }
 pub struct DynamixelSerial<P: ValidUartPinout<D>, D: UartDevice, DIR> {
-    serial: UartPeripheral<Enabled, D, P>,
-    baud_rate: u32,
-    dir_pin: DIR,
-    timer: Timer,
+	serial: UartPeripheral<Enabled, D, P>,
+	baud_rate: u32,
+	dir_pin: DIR,
+	timer: Timer,
 }
 
-impl<P, D, DIR> DynamixelSerial<P, D, DIR> where P: ValidUartPinout<D>, D: UartDevice {
-    pub fn new(serial: UartPeripheral<Enabled, D, P>, baud_rate: u32, dir_pin: DIR, timer: Timer) -> Self {
-        Self {
-            serial,
-            baud_rate,
-            dir_pin,
-            timer,
-        }
-    }
+impl<P, D, DIR> DynamixelSerial<P, D, DIR>
+where
+	P: ValidUartPinout<D>,
+	D: UartDevice,
+{
+	pub fn new(serial: UartPeripheral<Enabled, D, P>, baud_rate: u32, dir_pin: DIR, timer: Timer) -> Self {
+		Self {
+			serial,
+			baud_rate,
+			dir_pin,
+			timer,
+		}
+	}
 }
 
 impl<P, D, DIR> SerialPort for DynamixelSerial<P, D, DIR>
-    where P: ValidUartPinout<D>,
-    D: UartDevice,
-    DIR: OutputPin<Error = Infallible>
+where
+	P: ValidUartPinout<D>,
+	D: UartDevice,
+	DIR: OutputPin<Error = Infallible>,
 {
-    type Error = Error;
+	type Error = Error;
 
-    type Instant = Instant;
+	type Instant = Instant;
 
-    fn baud_rate(&self) -> Result<u32, Self::Error> {
-        Ok(self.baud_rate)
-    }
+	fn baud_rate(&self) -> Result<u32, Self::Error> {
+		Ok(self.baud_rate)
+	}
 
-    fn set_baud_rate(&mut self, _baud_rate: u32) -> Result<(), Self::Error> {
-        panic!("Changing baud rate is not supported");
-    }
+	fn set_baud_rate(&mut self, _baud_rate: u32) -> Result<(), Self::Error> {
+		panic!("Changing baud rate is not supported");
+	}
 
-    fn discard_input_buffer(&mut self) -> Result<(), Self::Error> {
-        Ok(())
-    }
+	fn discard_input_buffer(&mut self) -> Result<(), Self::Error> {
+		Ok(())
+	}
 
-    fn read(&mut self, buffer: &mut [u8], deadline: &Self::Instant) -> Result<usize, Self::Error> {
-        loop {
-            if deadline < &self.timer.get_counter() {
-                return Err(Error::Timeout);
-            }
-            match self.serial.read_raw(buffer) {
-                Err(nb::Error::Other(e)) => {
-                    return Err(Error::UartReadError(e.err_type))
-                }
-                Err(nb::Error::WouldBlock) => {}
-                Ok(x) => return Ok(x),
-            }
-        }
-    }
+	fn read(&mut self, buffer: &mut [u8], deadline: &Self::Instant) -> Result<usize, Self::Error> {
+		loop {
+			if deadline < &self.timer.get_counter() {
+				return Err(Error::Timeout);
+			}
+			match self.serial.read_raw(buffer) {
+				Err(nb::Error::Other(e)) => return Err(Error::UartReadError(e.err_type)),
+				Err(nb::Error::WouldBlock) => {},
+				Ok(x) => return Ok(x),
+			}
+		}
+	}
 
-    fn write_all(&mut self, buffer: &[u8]) -> Result<(), Self::Error> {
-        let _ = self.dir_pin.set_high();
-        self.serial.write_full_blocking(buffer);
-        let _ = block!(self.serial.flush());
-        let _ = self.dir_pin.set_low();
-        Ok(())
-    }
+	fn write_all(&mut self, buffer: &[u8]) -> Result<(), Self::Error> {
+		let _ = self.dir_pin.set_high();
+		self.serial.write_full_blocking(buffer);
+		let _ = block!(self.serial.flush());
+		let _ = self.dir_pin.set_low();
+		Ok(())
+	}
 
-    fn make_deadline(&self, timeout: Duration) -> Self::Instant {
-        let timeout = timeout.as_millis() as u64;
-        self.timer.get_counter() + timeout.millis()
-    }
+	fn make_deadline(&self, timeout: Duration) -> Self::Instant {
+		let timeout = timeout.as_millis() as u64;
+		self.timer.get_counter() + timeout.millis()
+	}
 
-    fn is_timeout_error(error: &Self::Error) -> bool {
-        matches!(error, Error::Timeout)
-    }
+	fn is_timeout_error(error: &Self::Error) -> bool {
+		matches!(error, Error::Timeout)
+	}
 }
